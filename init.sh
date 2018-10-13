f750eca2db6f21e3e4bd18bdd4acebd65b6d1407
inz2008 <37842107+inz2008@users.noreply.github.com>
2018-10-13 21:40:45 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-#sudo /etc/init.d/gunicorn restart
-﻿#sudo /etc/init.d/mysql start﻿
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start﻿
+
