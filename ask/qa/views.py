97bdd715235be8f82fa320c0284081b0b5a49e14
inz2008 <inz_2008@mail.ru>
2018-11-01 23:16:17 +0400

    123

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,8 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def test(request, *args, **kwargs):
+    #return render_to_response('index.html',{})
     return HttpResponse('OK')
 
