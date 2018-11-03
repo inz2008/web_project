1174878a425dae68e73bdd9066211d3cd56b127a
inz2008 <inz_2008@mail.ru>
2018-11-03 09:39:57 +0400

    11111

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,30 +1,22 @@
 """ask URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import include, path
-#from django.conf.urls import url
 
 urlpatterns = [
-    path('',include('qa.urls')),
-    path('admin/', admin.site.urls),
-#    path(r'^$', views.test()),
-#    path(r'^login/$', views.test()),
-#    path(r'^signup/$', views.test()),
-#    path(r'^question/*', views.test()),   #??????????
-#    path(r'^ask/$', views.test()),
-#    path(r'^popular/$', views.test()),
-#    path(r'^new/$', views.test()),
+    url(r'^',include('qa.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
